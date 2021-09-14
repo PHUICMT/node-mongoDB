303b07ebe35759e7c412d11500f314fdb0f405b5
PHUICMT <PHUICMT@github.com>
2021-09-14 18:36:00 +0700

    📝 Added body

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { connectMongo, Cat } = require("./mongo");
+const { connectMongo, User } = require("./mongo");
 
 const app = express();
 app.use(express.json());
@@ -7,15 +7,42 @@ app.use(express.json());
 const port = 3000;
 
 app.post("/cats", async (req, res) => {
-  console.log("POST !! " + req.body.name);
-  const newCats = new Cat({ name: req.body.name, age: req.body.age });
+  console.log("POST !! " + req.body);
+  const newUser = new User({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    birthday: req.body.birthday,
+    mobilephone: req.body.mobilephone,
+    address: {
+      address_number: req.body.address.address_number,
+      tambon: req.body.address.tambon,
+      district: req.body.address.district,
+      province: req.body.address.province,
+      postcode: req.body.address.postcode,
+    },
+    id_document: {
+      id_number: req.body.id_document.id_number,
+      id_picture: req.body.id_document.id_picture,
+    },
+    bank_document: {
+      bank_account_name: req.body.bank_document.bank_account_name,
+      bank_account_number: req.body.bank_document.bank_account_number,
+      bank_account_picture: req.body.bank_document.bank_account_picture,
+      bank_provider_name: req.body.bank_document.bank_provider_name,
+      bank_provider_brunch: req.body.bank_document.bank_provider_brunch,
+    },
+    site_section: {
+      site_name: req.body.site_section.site_name,
+      team_name: req.body.site_section.site_name,
+      advisor_name: req.body.site_section.site_name,
+    },
+    salary: {
+      salary_per_day: req.body.salary.salary_per_day,
+      salary_part_time: req.body.salary.salary_part_time,
+    },
+    discord_account: req.body.discord_account,
+  });
   newCats.save().then(() => res.send("posted!!"));
-});
-
-app.get("/cats", async (req, res) => {
-  let results = await Cat.find({}).exec();
-  console.log(results);
-  res.send("Hello RinChan");
 });
 
 connectMongo().then(() => {
--- mongo.js
+++ mongo.js
@@ -3,6 +3,39 @@ const connectMongo = () => {
   return mongoose.connect("mongodb://localhost:27017/test");
 };
 
-const Cat = mongoose.model("Cat", { name: String, age: Number });
+const User = mongoose.model("User", {
+  first_name: String,
+  last_name: String,
+  birthday: Date,
+  mobilephone: String,
+  address: {
+    address_number: String,
+    tambon: String,
+    district: String,
+    province: String,
+    postcode: String,
+  },
+  ID_document: {
+    ID_number: String,
+    ID_picture: String,
+  },
+  bank_document: {
+    bank_account_name: String,
+    bank_account_number: String,
+    bank_account_picture: String,
+    bank_provider_name: String,
+    bank_provider_brunch: String,
+  },
+  site_section: {
+    site_name: String,
+    team_name: String,
+    advisor_name: String,
+  },
+  salary: {
+    salary_per_day: Number,
+    salary_part_time: Number,
+  },
+  discord_account: String,
+});
 
-module.exports = { connectMongo, Cat };
+module.exports = { connectMongo, User };
